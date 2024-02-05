64dd8038379a262162ca2d98489a7e03f3a22cee
Kris <kkaliszuk@wp.pl>
2024-02-05 23:37:07 +0100

    Test

---
+++ Skrypt1/main.lua
@@ -0,0 +1,26 @@
+print('Hello, World!')
+fruit = 0
+apples = 2
+oranges = 3
+fruit = apples + oranges   --get the total fruit
+fruit=apples + oranges   --get the total fruit
+fruit=apples+oranges--get the total fruit
+print(fruit)
+
+-- Funkcja do podwojenia liczby
+function podwoj(liczba)
+    return liczba * 2
+end
+
+-- Utworzenie tablicy liczb
+local liczby = {1, 2, 3, 4, 5}
+
+-- Pętla do podwojenia każdej liczby w tablicy
+for i, v in ipairs(liczby) do
+    liczby[i] = podwoj(v)
+end
+
+-- Wypisanie zmodyfikowanej tablicy
+for i, v in ipairs(liczby) do
+    print("Liczba po podwojeniu: " .. v)
+end
